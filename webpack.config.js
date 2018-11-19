b7950ab4f3edb6ece53e42320b4535654a711b8d
gitHubUserGuy <9242tangyawei@sina.com>
2018-11-19 21:00:19 +0800

    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,59 @@
 var path=require('path');
 var webpack=require('webpack');
-var PORT = 8080;
-var HOSTNAME = '127.0.0.1';
 var ExtractTextPlugin = require("extract-text-webpack-plugin"); 
 var CommonsChunkPlugin = require("webpack/lib/optimize/CommonsChunkPlugin");
 
-// var proxy=require("http-proxy-middleware");
+var PORT = 8080;
+var HOSTNAME = '127.0.0.1';
+
+var env = process.env.NODE_ENV
+var envPlugins = env==='development'?
+	[
+		new webpack.HotModuleReplacementPlugin(),
+		new CommonsChunkPlugin({
+           name: 'vendor',
+           filename: 'vendor.js'
+        })
+    ]:
+    [
+		new webpack.optimize.CommonsChunkPlugin({
+           name: 'vendor',
+           filename: 'vendor.js'
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+	      compress: {
+	        warnings: false
+	      }
+	    }),
+	    new webpack.DefinePlugin({
+	      'process.env.NODE_ENV': JSON.stringify(env)
+	    })
+    ]
+
 
 module.exports={
-	devtool: 'eval-source-map',
+	devtool: env==='development'? 'eval-source-map':false,
 	entry: {
-		index: [
-			'react-hot-loader/patch',
-			'webpack-dev-server/client?http://'+HOSTNAME+':'+PORT,
-			// 'webpack/hot/only-dev-server',
-			'./src/pages/index.js'
-		],
-		vendor: ['react', 'react-dom', 'react-router', 'jquery', 'react-router-dom']
+		index: env==='development'?
+			[
+				'react-hot-loader/patch',
+				'webpack-dev-server/client?http://'+HOSTNAME+':'+PORT,
+				'./src/pages/index.js'
+			] : './src/pages/index.js',
+		vendor: ['react', 'react-dom', 'jquery', 'react-router-dom']
 	},
 	output:{
-		// path: path.resolve(__dirname,'./build'),
+		path: path.resolve(__dirname,'./dist'),
 		filename:'bundle.js',
 		publicPath: '/'
 	},
-	devServer: {
-		contentBase: "./",
+	devServer: env==='development'? {
+		contentBase: './dist',
 		inline: true,
 		historyApiFallback: true,
 		port:PORT,
 		host: HOSTNAME
-	},
+	}:{},
 	resolve: {
 		extensions: ['.js', '.jsx', '.json']
 	},
@@ -67,12 +91,8 @@ module.exports={
 		]
 	},
 	plugins: [
-		new webpack.HotModuleReplacementPlugin(),
 		new ExtractTextPlugin('style.css'),
-		new CommonsChunkPlugin({
-           name: 'vendor',
-           filename: 'vendor.js'
-        })
+		...envPlugins
 	]
 }
 
